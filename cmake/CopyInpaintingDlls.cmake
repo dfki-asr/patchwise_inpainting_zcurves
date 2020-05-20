2918dca28282faa19b8c565b480de8430e1b32b4
Tim Dahmen <Tim.Dahmen@dfki.de>
2020-05-20 14:32:40 +0200

    cmake cleanup and dependency unwrangling

--- cmake/CopyInpaintingDlls.cmake
+++ cmake/CopyInpaintingDlls.cmake
@@ -1,53 +1,34 @@
-macro(CopyDlls)
+macro(CopyInpaintingDlls)
     # copy necessary dependency DLLs to destination folder
     if(MSVC)
         file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/bin/Debug/")
-        add_custom_command(
-            TARGET ${PROJECT_NAME} 
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy
-                "${MKL_ROOT_DIR}/lib/mkl_core.dll"
-                "${MKL_ROOT_DIR}/lib/mkl_sequential.dll"
-                "${MKL_ROOT_DIR}/lib/mkl_avx2.dll"
-                "${MKL_ROOT_DIR}/lib/mkl_def.dll"
-                "${DAAL_ROOT_DIR}/bin/daal_core.dll"
-                "${DAAL_ROOT_DIR}/bin/daal_thread.dll"
-                "${DAAL_ROOT_DIR}/bin/tbb.dll"
-                "${DAAL_ROOT_DIR}/bin/tbbmalloc.dll"
-                "${CMAKE_BINARY_DIR}/bin/Debug/"
-        )
-        
-        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/bin/Release/")
-        add_custom_command(
-            TARGET ${PROJECT_NAME} 
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy
-            "${MKL_ROOT_DIR}/lib/mkl_core.dll"
-            "${MKL_ROOT_DIR}/lib/mkl_sequential.dll"
-            "${MKL_ROOT_DIR}/lib/mkl_avx2.dll"
-            "${MKL_ROOT_DIR}/lib/mkl_def.dll"
-            "${DAAL_ROOT_DIR}/bin/daal_core.dll"
-            "${DAAL_ROOT_DIR}/bin/daal_thread.dll"
-            "${DAAL_ROOT_DIR}/bin/tbb.dll"
-            "${DAAL_ROOT_DIR}/bin/tbbmalloc.dll"
-            "${CMAKE_BINARY_DIR}/bin/Release/"
-        )
-        
-        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/bin/RelWithDebInfo/")
-        add_custom_command(
-            TARGET ${PROJECT_NAME} 
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy
-            "${MKL_ROOT_DIR}/lib/mkl_core.dll"
-            "${MKL_ROOT_DIR}/lib/mkl_sequential.dll"
-            "${MKL_ROOT_DIR}/lib/mkl_avx2.dll"
-            "${MKL_ROOT_DIR}/lib/mkl_def.dll"
-            "${DAAL_ROOT_DIR}/bin/daal_core.dll"
-            "${DAAL_ROOT_DIR}/bin/daal_thread.dll"
-            "${DAAL_ROOT_DIR}/bin/tbb.dll"
-            "${DAAL_ROOT_DIR}/bin/tbbmalloc.dll"
-            "${CMAKE_BINARY_DIR}/bin/RelWithDebInfo/"
-        )
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_core.dll"       "${CMAKE_BINARY_DIR}/bin/Debug/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_sequential.dll" "${CMAKE_BINARY_DIR}/bin/Debug/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_avx2.dll"       "${CMAKE_BINARY_DIR}/bin/Debug/" )
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_def.dll"        "${CMAKE_BINARY_DIR}/bin/Debug/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/daal_core.dll"     "${CMAKE_BINARY_DIR}/bin/Debug/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/daal_thread.dll"   "${CMAKE_BINARY_DIR}/bin/Debug/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/tbb.dll"           "${CMAKE_BINARY_DIR}/bin/Debug/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/tbbmalloc.dll"     "${CMAKE_BINARY_DIR}/bin/Debug/")       
 
+        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_core.dll"       "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_sequential.dll" "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_avx2.dll"       "${CMAKE_BINARY_DIR}/bin/Release/" )
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_def.dll"        "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/daal_core.dll"     "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/daal_thread.dll"   "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/tbb.dll"           "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/tbbmalloc.dll"     "${CMAKE_BINARY_DIR}/bin/Release/")       
+
+        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_core.dll"       "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_sequential.dll" "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_avx2.dll"       "${CMAKE_BINARY_DIR}/bin/Release/" )
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${MKL_ROOT_DIR}/lib/mkl_def.dll"        "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/daal_core.dll"     "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/daal_thread.dll"   "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/tbb.dll"           "${CMAKE_BINARY_DIR}/bin/Release/")
+        add_custom_command( TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${DAAL_ROOT_DIR}/bin/tbbmalloc.dll"     "${CMAKE_BINARY_DIR}/bin/Release/")       
     endif(MSVC)
 endmacro()
